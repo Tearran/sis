9c1f73ddfdbe659073ecf8c7fe0e1827ae6f8a78
Joseph Turner <Tearran@users.noreply.github.com>
2023-04-05 14:54:32 -0700

    Update run.sh

--- senpi/run.sh
+++ senpi/run.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-## Change to the directory where this script is located
-#cd "$(dirname "$0")"
-#dir="$(pwd)"
-##start a python3 simple server
-#$(nohup python3 -m http.server 8000 --dir "$(pwd)" &) &
 
 # Change to the directory where this script is located
 cd "$(dirname "$0")"
 dir="$(pwd)"
 
-# Check if http.server is already running
-if pgrep -f "python3 -m http.server 8000" > /dev/null; then
-  echo "http.server is already running"
-else
-  # Start http.server
-  nohup python3 -m http.server 8000 --dir "$(pwd)" > /dev/null 2>&1 &
-  echo "http.server started"
+# Check if i2c bus is enabled
+i2c_status=$(sudo raspi-config nonint get_i2c)
+if [ "$i2c_status" -eq 1 ]; then
+  sudo raspi-config nonint do_i2c 0
 fi
 
-# Rest of your script goes here
+# Check if http.server is already running
+if pgrep -f "python3 -m http.server 8000" > /dev/null; then
+	echo "http.server is already running"
+else
+	# Start http.server
+	nohup python3 -m http.server 8000 --dir "$(pwd)" > /dev/null 2>&1 &
+	echo "http.server started"
+fi
 
-
+# Loop the sensors readings
 while true; do
     # load the eviromental data scripts
     icm20948_sample=$(python3 -c "$(cat $dir/modules/icm20948_data.py)")
@@ -34,14 +33,8 @@ while true; do
 
 
 	# Save the data to a JSON file
-#	echo $bme680_sample > bme680.json
-#	echo $bmp280_sample > bmp280.json
 #   echo $imc20948_sample > $imc20948.json
 #	echo $system_info > sysinfo.json
-
-#	echo '{"imc20948":'$imc20948_sample' , "system info":'$system_info'}' > senpi.json
-#   echo '{"bme680":'$bme680_sample' , "bmp280":'$bmp280_sample' , "system info":'$system_info'}' > senpi.json
     echo '{"icm20948":'$icm20948_sample' , "system info":'$system_info'}'  > senpi.json
 
 done
-
