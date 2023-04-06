3c27e2ed32a061e36430d7798df7592c15e97129
Joseph Turner <Tearran@users.noreply.github.com>
2023-04-06 03:41:08 -0700

    Update run.sh

--- senpi/run.sh
+++ senpi/run.sh
@@ -1,56 +1,21 @@
 #!/bin/bash
 
-
 # Change to the directory where this script is located
 cd "$(dirname "$0")"
 dir="$(pwd)"
 
-# Check if i2c bus is enabled
-i2c_status=$(sudo raspi-config nonint get_i2c)
-if [ "$i2c_status" -eq 1 ]; then
-  sudo raspi-config nonint do_i2c 0
-fi
-
-# Check if smbus and smbus2 modules are installed
-python3 -c "import smbus, smbus2" 2>/dev/null
-if [ $? -eq 0 ]; then
-  echo "smbus and smbus2 are installed."
-else
-  # Display a message using whiptail
-  if (whiptail --title "Missing Python modules" --yesno "smbus and smbus2 modules are required for this script to run. Install them now?" 10 60) then
-    pip3 install smbus smbus2
-    echo "smbus and smbus2 have been installed."
-  else
-    echo "smbus and smbus2 are not installed."
-    exit 1
-  fi
-fi
-
 # Check if http.server is already running
 if pgrep -f "python3 -m http.server 8000" > /dev/null; then
-	echo "http.server is already running"
+  echo "http.server is already running"
 else
-	# Start http.server
-	nohup python3 -m http.server 8000 --dir "$(pwd)" > /dev/null 2>&1 &
-	echo "http.server started"
+  # Start http.server
+  nohup python3 -m http.server 8000 --dir "$(pwd)" > /dev/null 2>&1 &
+  echo "http.server started"
 fi
 
-# Loop the sensors readings
 while true; do
     # load the eviromental data scripts
     icm20948_sample=$(python3 -c "$(cat $dir/modules/icm20948_data.py)")
-	# Get CPU temperature
-	cpu_temp=$( /usr/bin/vcgencmd measure_temp | awk -F "[=']" '{print($2)}' )
-	# Get free RAM
-	free_mem=$(free -m | awk 'NR==2{printf "\"total_mem\": %s, \"free_mem\": %s", $2,$4}')
-	# Output results in JSON format
-	system_info='{"cpu_temp": '$cpu_temp', '$free_mem'}' #> cpu.json
-
-
-	# Save the data to a JSON file
-#   echo $imc20948_sample > $imc20948.json
-#	echo $system_info > sysinfo.json
-    echo '{"icm20948":'$icm20948_sample' , "system info":'$system_info'}'  > senpi.json
-
+	echo -e '{"imc20948":'$imc20948_sample' , "system info":'$system_info'}' > senpi.json
+#   echo '{"bme680":'$bme680_sample' , "bmp280":'$bmp280_sample' , "system info":'$system_info'}' > senpi.json
 done
-
